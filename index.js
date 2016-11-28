8a26aad1e826bfbc70eed52cdf016341cf9aaf56
Ian Mitchell <ianm@zillowgroup.com>
2016-11-27 22:24:34 -0800

    Support For Older Node Versions
    Per request

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 fs = require('fs')
 
-module.exports = () => {
-  if (process.platform.toLowerCase() === 'linux' && 
+function isWindowsBash() {
+  if (process.platform.toLowerCase() === 'linux' &&
       process.env.SHELL.toLowerCase() === '/bin/bash') {
     try {
-      const data = fs.readFileSync('/proc/version', 'utf-8');    
-      return data.toLowerCase().includes('microsoft');
-    } catch (err) {    
+      var data = fs.readFileSync('/proc/version', 'utf-8');
+      return data.toLowerCase().indexOf('microsoft') > -1;
+    } catch (err) {
       return false;
     }
   }
 
   return false;
-};
+}
+
+module.exports = isWindowsBash;
