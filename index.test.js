5d7621725fcf9cb977dcaace4a932a66974c3b71
Ian Mitchell <ianm@zillowgroup.com>
2016-11-28 09:04:26 -0800

    Test Cleanup

--- index.test.js
+++ index.test.js
@@ -7,31 +7,33 @@ const PROC_VERSION_STUBS = {
   DEBIAN: "Linux version 3.16.0-4-amd64 (debian-kernel@lists.debian.org) (gcc version 4.8.4 (Debian 4.8.4-1) ) #1 SMP Debian 3.16.36-1+deb8u2 (2016-10-19)",
 };
 
-function setProcessVars(platform = 'Linux', shell = '/bin/bash') {
-  process.platform = platform;
-  process.env.SHELL = shell;
+function resetProcessVars() {
+  process.platform = 'Linux';
+  process.env.SHELL = '/bin/bash';
 }
 
 describe('Handles operating systems correctly', () => {
   test('Should only trigger on Linux', () => {
-    setProcessVars('darwin');
+    process.platform = 'darwin';
     expect(isWindowsBash()).toBe(false);
+
+    resetProcessVars();
   });
 
   test('Should only trigger on Bash', () => {
-    setProcessVars('Linux', '/bin/zsh');
+    process.env.SHELL = '/bin/zsh';
     expect(isWindowsBash()).toBe(false);
+
+    resetProcessVars();
   });
 
   test('Should return true on WSL', () => {
-    setProcessVars();
     fs.readFileSync = jest.fn().mockReturnValue(PROC_VERSION_STUBS.WINDOWS);
 
     expect(isWindowsBash()).toBe(true);
   });
 
   test('Should return false on non-WSL result', () => {
-    setProcessVars();
     fs.readFileSync = jest.fn().mockReturnValue(PROC_VERSION_STUBS.DEBIAN);
 
     expect(isWindowsBash()).toBe(false);
@@ -40,7 +42,6 @@ describe('Handles operating systems correctly', () => {
 
 describe('Handles edge cases correctly', () => {
   test("Should return false if file doesn't exist", () => {
-    setProcessVars();
     fs.readFileSync = jest.fn().mockImplementation(() => {
       throw new Error("ENOENT: no such file or directory, open '/proc/version'");
     });
@@ -49,7 +50,10 @@ describe('Handles edge cases correctly', () => {
   });
 
   test("Should return false without a process variable", () => {
+    const proc = process;
     process = {};
     expect(isWindowsBash()).toBe(false);
+
+    process = proc;
   })
 });
